83ff76ebba62fcc0e09e35ba79da55c0e4381c89
sunshinetoyou <victorr0618@naver.com>
2023-12-03 12:35:05 +0900

    car move mechanism clear
    1. length
        한번에 움직이는 픽셀 거리
        현재 4로 고정
    2. theta
        한번에 돌아가는 각도
        현재 30으로 고정

---
+++ src/main/java/com/danyeon/testController.java
@@ -0,0 +1,121 @@
+package com.danyeon;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.animation.AnimationTimer;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Bounds;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.shape.Rectangle;
+
+public class testController implements Initializable {
+    @FXML private Rectangle car;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        new Car(car);
+        update();
+    }
+
+    public void update() {
+        AnimationTimer timer = new AnimationTimer() {
+            
+            private long lastUpdate = 0;
+            @Override
+            public void handle(long now) {
+                if (now - lastUpdate >= 25_000_000) {   // 1ms = 1_000_000 ns
+                    // update 할 것들
+                    // System.out.println(car.getTranslateX());
+                    lastUpdate = now;
+                }
+            }
+        };
+        timer.start();
+
+//         Thread cameraThread = new Thread() {
+// 			@Override
+// 			public void run() {
+
+// 				AnimationTimer timer = new AnimationTimer() {
+// 					@Override
+// 					public void handle(long now) {
+// 						// moveCamera();
+// //						System.out.println("CAMERA");
+// 					}
+// 				};
+// 				timer.start();
+// 			}
+// 		};
+// 		Platform.runLater(cameraThread);
+// 		cameraThread.setDaemon(true);
+// 		cameraThread.start();
+    }
+}
+
+
+class Car {
+    
+    @FXML private Rectangle car;
+
+    private double length = 4;
+    private double theta = 30;
+
+    public Car(Rectangle rec) {
+        this.car = rec;
+
+        car.setOnMouseClicked(e -> {
+            System.out.println(getBound());
+        });
+
+        car.setOnKeyPressed((KeyEvent event) -> {
+            // System.out.println(event.getCode());
+            switch (event.getCode()) {
+                case RIGHT: handle(-theta);
+                            // System.out.println(car.getRotate());
+                            break;
+                case LEFT:  handle(theta);
+                            // System.out.println(car.getRotate());
+                            break;
+                case UP:    axel(-length);
+                            break;
+                case DOWN:  axel(length);
+                            break;
+                case SPACE: reset();
+                            break;
+                default:    System.out.println("Not Correct KeyCode");
+                            break;
+            }
+        });
+    }
+
+    // x, y 값 움직이기
+
+    // 앞으로 움직이기
+    public void axel(double length) {
+        // System.out.println("각도 : " + car.getRotate());
+        // System.out.println("ang " + (360 + car.getRotate()) % 360 + 90 );
+        // System.out.println("cos " + Math.cos(Math.toRadians((360 + car.getRotate()) % 360 + 90)));
+        // System.out.println("sin " + Math.sin(Math.toRadians((360 + car.getRotate()) % 360 + 90)));
+        // System.out.println();
+
+        car.setTranslateX(car.getTranslateX() + length * Math.cos(Math.toRadians((360 + car.getRotate()) % 360 + 90)));
+        car.setTranslateY(car.getTranslateY() + length * Math.sin(Math.toRadians((360 + car.getRotate()) % 360 + 90)));
+    }
+
+    // 옆으로 움직이기
+    public void handle(double angle) {
+        car.setRotate(car.getRotate() - angle);;
+    }
+
+    public void reset() {
+        car.setTranslateX(0);
+        car.setTranslateY(0);
+        car.setRotate(0);
+    }
+
+    public Bounds getBound() {
+        return car.getBoundsInLocal();
+    }
+}
