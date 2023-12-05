0adcfa77b4f57017e8509c24083676e8aec4a12e
Yeonsu Jung <victorr0618@naver.com>
2023-12-05 15:51:13 +0900

    클립 만들고 scene 에 영향 받게 만드는 것 까지 함

--- src/main/java/com/danyeon/testMoveController.java
+++ src/main/java/com/danyeon/testMoveController.java
@@ -3,6 +3,7 @@ package com.danyeon;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -19,27 +20,27 @@ public class testMoveController implements Initializable{
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-        // moveClip();
-        System.out.println("CAR : " + car.getX()+ " width : " + car.getWidth());
-        System.out.println("clip : " + clip.getWidth());
-        System.out.println("clampRange : " + (clip.getWidth() - car.getWidth())/2);
-        // clip.layoutXProperty().bind(Bindings.createDoubleBinding(
-        //     () -> clampRange(getRealX()-((clip.getWidth() - car.getWidth())/2), 0, map.getWidth()-clip.getWidth()),
-        //     car.translateXProperty()
-        // ));
-        
+
+        Platform.runLater(() ->
+            moveClip()
+        );
+        moveMap();
         car.setOnKeyPressed((KeyEvent event) -> {
             switch (event.getCode()) {
-                case RIGHT: car.setX(car.getX() + 30);
+                case RIGHT: car.setTranslateX(car.getTranslateX() + 30);
                             break;
-                case LEFT:  car.setX(car.getX() - 30);
+                case LEFT:  car.setTranslateX(car.getTranslateX() - 30);
                             break;
-                case UP:    car.setY(car.getY() - 30);
+                case UP:    car.setTranslateY(car.getTranslateY() - 30);
                             break;
-                case DOWN:  car.setY(car.getY() + 30);
+                case DOWN:  car.setTranslateY(car.getTranslateY() + 30);
                             break;
-                default:    // System.out.println("Not Correct Value");
-                            System.out.println(car.getX());
+                default:    System.out.println("Not Correct Value");
+                            // System.out.println("CAR : " + car.getX() + " " + car.getTranslateX());
+                            // System.out.println("CLIP : " + clip.getTranslateX());
+                            // System.out.println("CLAMPRANGE : " +clampRange(getRealX()-(clip.getWidth() - car.getWidth())/2, 0, map.getWidth()-clip.getWidth()));
+                            // System.out.println("MAP : " + map.getLayoutX() + " " + map.getLayoutY());
+                            System.out.println(App.getScene());
                             break;
             }
         });
@@ -50,19 +51,39 @@ public class testMoveController implements Initializable{
     }
 
     public void moveClip() {
-        // Scene scene = getScene();
+        Scene scene = App.getScene();
+        if (scene != null) {
+            clip.widthProperty().bind(scene.widthProperty());
+            clip.heightProperty().bind(scene.heightProperty());
+        }
 
         clip.xProperty().bind(Bindings.createDoubleBinding(
-            () -> clampRange(car.getX()-((clip.getWidth() - car.getWidth())/2), 0, map.getWidth()-clip.getWidth()),
-            car.xProperty()
+            () -> clampRange(getRealX()-(clip.getWidth() - car.getWidth())/2, 0, map.getWidth()-clip.getWidth()),
+            car.translateXProperty()
         ));
-        clip.yProperty();
+        clip.yProperty().bind(Bindings.createDoubleBinding(
+            ()-> clampRange(getRealY()-(clip.getHeight() - car.getHeight())/2, 0, map.getHeight()-clip.getHeight()),
+            car.translateYProperty()
+        ));
+    }
+
+    public void moveMap() {
+        map.layoutXProperty().bind(Bindings.createDoubleBinding(
+            ()-> -car.getTranslateX(),
+            car.translateXProperty()
+        ));
+        map.layoutYProperty().bind(Bindings.createDoubleBinding(
+            ()-> -car.getTranslateY(),
+            car.translateYProperty()
+        ));
     }
 
     private double clampRange(double value, double min, double max) {
+        // System.out.println("VALUE "+value+"MIN "+min+"MAX "+max);
+        if (max < min ) return value; // map.width 가 0이 되어서 max 값이 -200 되는 경우 예외처리
         if (value < min) return min;
-        if (value > max) return max ;
-        return value ;
+        if (value > max) return max;
+        return value;
     }
 
     // public Rectangle makeClip() {
@@ -88,7 +109,11 @@ public class testMoveController implements Initializable{
     //     return clip;
     // }
     
-    // public double getRealX() {
-    //     return car.getLayoutX() + car.getTranslateX();
-    // }
+    public double getRealX() {
+        return car.getX() + car.getTranslateX();
+    }
+
+    public double getRealY() {
+        return car.getY() + car.getTranslateY();
+    }
 }
