1de0dbdde042a815c6fbba27e08ea8064de3361f
sunshinetoyou <victorr0618@naver.com>
2023-12-06 20:53:16 +0900

    주석처리 + 수정

--- src/main/java/com/danyeon/testMoveController.java
+++ src/main/java/com/danyeon/testMoveController.java
@@ -18,14 +18,17 @@ public class testMoveController implements Initializable{
     @FXML private Rectangle car;
     @FXML private Rectangle car2;
     @FXML private Pane map;
-    @FXML private Rectangle clip;
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
+
+        // scene 생성된 후에 작동
+        // 이렇게 안하면 scene에 null 값이 들어감
         Platform.runLater(
-            () -> moveMap(car)
+            () -> moveScreen(car)
         );
 
+        // 키보드 이벤트 핸들러
         car.setOnKeyPressed((KeyEvent event) -> {
             switch (event.getCode()) {
                 case RIGHT: car.setTranslateX(car.getTranslateX() + 30);
@@ -36,17 +39,20 @@ public class testMoveController implements Initializable{
                             break;
                 case DOWN:  car.setTranslateY(car.getTranslateY() + 30);
                             break;
-                default:    // System.out.println("Not Correct Value");
-                            moveMap(car2);
-                            testPrint();
+                default:    System.out.println("Not Correct Value");
+                            // moveScreen(car2);
+                            // testPrint();
                             break;
             }
         });
     }
 
-    public void moveMap(Rectangle center) {
+    public void moveScreen(Rectangle center) {
         Scene scene = App.getScene();
-
+        
+        // rootNode(=Scene) 의 layout 값을 변경해서 맵 이동 구현
+        // car.translate : car 가 이동할 때
+        // scene.width   : 창 사이즈가 바뀔 때 -> rootNode.layoutX를 재조정
         rootNode.layoutXProperty().bind(Bindings.createDoubleBinding(
             () -> -(getRealX(center)-(scene.getWidth() - car.getWidth())/2),
             center.translateXProperty(), scene.widthProperty()
@@ -56,23 +62,16 @@ public class testMoveController implements Initializable{
             center.translateYProperty(), scene.heightProperty()
         ));
     }
-
-    // private double clampRange(double value, double min, double max) {
-    //     // System.out.println("VALUE "+value+"MIN "+min+"MAX "+max);
-    //     if (max < min ) return value; // map.width 가 0이 되어서 max 값이 -200 되는 경우 예외처리
-    //     if (value < min) return min;
-    //     if (value > max) return max;
-    //     return value;
-    // }
     
+    // 객체의 위치와 이동값을 합해서 반환
     public double getRealX(Rectangle car) {
         return car.getX() + car.getTranslateX();
     }
-
     public double getRealY(Rectangle car) {
         return car.getY() + car.getTranslateY();
     }
     
+    // 테스트 코드
     public void testPrint() {
         System.out.println("ROOTNODE.layoutX : " + rootNode.getLayoutX() +"\tROOTNODE.width : " + rootNode.getWidth());
         System.out.println("CAR.x : " + car.getX() + "\tCAR.translateX : " + car.getTranslateX());
@@ -80,6 +79,6 @@ public class testMoveController implements Initializable{
         // System.out.println("SCENE.width :" + App.getScene().getWidth());
         System.out.println("setRootNodeX" + (getRealX(car)-(App.getScene().getWidth() - car.getWidth())/2));
         // moveMap(car2);
-        // System.out.println("CAR2.x :" + car2.getX() + "\tCAR2.translateX : "+ car.getTranslateX());
+        // System.out.println("CAR2.x :" + car2.getX() +    "\tCAR2.translateX : "+ car.getTranslateX());
     }
 }
